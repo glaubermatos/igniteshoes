01ca61eeb7c6a8ebbb32f0bf64bcafe673fe3650
glaubermatos <glaub.oliveira@hotmail.com>
2023-08-22 10:15:41 -0300

    push notifications

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'react-native';
-import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal, {  } from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { tagUserInfoCreate } from './src/notifications/notificationsTags'
@@ -11,13 +11,12 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { useEffect } from 'react';
 
 OneSignal.setAppId("0b0a5cca-8c67-4c0f-a0b5-66f4d4e25267");
 
 //apenas para Iphone
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response)
-})
+OneSignal.promptForPushNotificationsWithUserResponse()
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -25,6 +24,24 @@ export default function App() {
   //identifica esse dispositivo pelo email do usuário
   //ou cria tags com as informações do usuário logado que identifica o dispositivo
   tagUserInfoCreate({email: 'glaub.oliveira@hotmail.com', name: 'Glauber'})
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+      const {actionId} = response.action as any
+
+      switch (actionId) {
+        case '1':
+          return console.log('Ver todas');     
+        case '2':
+          return console.log('Ver notificação');      
+      
+        default:
+          return console.log('Não foi clicado em nenhuma ação');
+      }
+    })
+
+    return () => unsubscribe;
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,41 @@
+import { useEffect, useState } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 
+import { Notification } from '../components/Notification';
+
 export function Routes() {
   const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification>()
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const unsubscribe = OneSignal
+    .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
+      const response = notificationReceivedEvent.getNotification();
+      
+      setNotification(response)
+    })
+
+    return unsubscribe;
+  }, [])
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {
+        notification?.title && 
+          <Notification 
+            data={notification}
+            onClose={() => {setNotification(undefined)}} 
+          />
+      }
     </NavigationContainer>
   );
 }
